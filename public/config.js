7e8fd068bfd94442e99a218daea78d039307cc4a
yaswanth-gupta07 <yaswanth_gupta@srmap.edu.in>
2025-12-18 12:14:05 +0530

    Update BASE_URL for production API

--- public/config.js
+++ public/config.js
@@ -3,6 +3,6 @@ const API_CONFIG = {
     // Change this to your backend URL
     // For local development: http://localhost:5000
     // For production: your production API URL
-    BASE_URL: 'http://localhost:5000/api'
+    BASE_URL: 'https://unihub-l15n.onrender.com'
 };
 
